304f6471e04aae5ac8b8024f5f51a6d69f71e1f6
Jocsan Morera <joey_morera13@hotmail.com>
2023-01-28 13:20:03 -0600

    Portfolio version 1.0

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+//Hamburger Menu
+
+const nav = document.querySelector(".navbar");
+const menu = document.querySelector("#hamburger");
+
+menu.addEventListener("click", () => {
+    nav.classList.toggle("open");
+});
+
+
+
+
+
+
+
+
+
+
